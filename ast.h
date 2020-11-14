6cd24439045cd4e7a1146091956bd1ff4a151376
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-11-14 08:51:32 +0100

    rename

--- ast.h
+++ ast.h
@@ -17,7 +17,7 @@ static const char type_to_str[][20] = {
     [TYPE_ANY] = "Any",       [TYPE_UNIT] = "Unknown",
     [TYPE_BOOL] = "Bool",     [TYPE_CHAR] = "Char",
     [TYPE_BYTE] = "Byte",     [TYPE_INT] = "Int",
-    [TYPE_SHORT] = "Short",   [TYPE_LONG] = "Int64",
+    [TYPE_SHORT] = "Short",   [TYPE_LONG] = "Long",
     [TYPE_STRING] = "String", [TYPE_BUILTIN_PRINTLN] = "BuiltinPrint",
 };
 
