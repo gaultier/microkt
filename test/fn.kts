20e7ee893b13c25303d136f6ae9c700c75c4842a
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-11-23 21:13:56 +0100

    expand test

--- test/fn.kts
+++ test/fn.kts
@@ -11,3 +11,11 @@ one_stmt_no_args() // expect: one_stmt_no_args
 
 println("B") // expect: B
 
+fun one_stmt_no_args1() {
+  println("one_stmt_no_args1")
+  one_stmt_no_args()
+}
+
+one_stmt_no_args1() // expect: one_stmt_no_args1
+// expect: one_stmt_no_args
+
