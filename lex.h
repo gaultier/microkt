321cfcfe2c882beead9de30e7c38b07551b91d1e
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-12-05 08:03:22 +0100

    asserts

--- lex.h
+++ lex.h
@@ -151,6 +151,7 @@ static char lex_advance(lexer_t* lexer, int* col) {
     PG_ASSERT_COND((void*)lexer, !=, NULL, "%p");
     PG_ASSERT_COND((void*)lexer->lex_source, !=, NULL, "%p");
     PG_ASSERT_COND((void*)col, !=, NULL, "%p");
+    PG_ASSERT_COND(lexer->lex_index, <=, lexer->lex_source_len, "%d");
 
     lexer->lex_index += 1;
     *col += 1;
@@ -167,8 +168,10 @@ static bool lex_is_at_end(const lexer_t* lexer) {
 static char lex_peek(const lexer_t* lexer) {
     PG_ASSERT_COND((void*)lexer, !=, NULL, "%p");
     PG_ASSERT_COND((void*)lexer->lex_source, !=, NULL, "%p");
+
     if (lex_is_at_end(lexer)) return '\0';
 
+    PG_ASSERT_COND(lexer->lex_index, <, lexer->lex_source_len, "%d");
     return lexer->lex_source[lexer->lex_index];
 }
 
