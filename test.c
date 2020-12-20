cbffb51a9085b7a7c98e64b4434874876ebcabd4
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-12-20 21:41:51 +0100

    Read source

--- test.c
+++ test.c
@@ -6,27 +6,41 @@
 #include "common.h"
 
 bool test() {
-    /* const char* source_file_name = "./tests/string.kts"; */
-    /* FILE* source_file = NULL;  // FIXME */
+    const char* source_file_name = "./tests/string.kts";
+    FILE* source_file = fopen(source_file_name, "r");
+    if (!source_file) {
+        fprintf(stderr, "Error reading file `%s`: err=%d errno=%s\n",
+                source_file_name, errno, strerror(errno));
+        return false;
+    }
+    char source_file_content[1 << 11];
+    size_t read_bytes =
+        fread(source_file_content, 1, sizeof(source_file_content), source_file);
+    if (ferror(source_file)) {
+        fprintf(stderr, "Error reading content of `%s`: errno=%d err=%s\n",
+                source_file_name, errno, strerror(errno));
+        return false;
+    }
+    CHECK(read_bytes, <=, sizeof(source_file_content), "%zu");
 
-    const char argv0[] = "./tests/string.exe";
-    FILE* exe_process = popen(argv0, "r");
+    const char argv[] = "./tests/string.exe";
+    FILE* exe_process = popen(argv, "r");
     if (exe_process == NULL) {
-        fprintf(stderr, "Error launching `%s`: errno=%d err=%s\n", argv0, errno,
+        fprintf(stderr, "Error launching `%s`: errno=%d err=%s\n", argv, errno,
                 strerror(errno));
         return false;
     }
 
-    char buf[1 << 11];
-    const size_t read_bytes = fread(buf, 1, sizeof(buf), exe_process);
+    char output[1 << 11];
+    read_bytes = fread(output, 1, sizeof(output), exe_process);
     if (ferror(exe_process)) {
-        fprintf(stderr, "Error reading output of `%s`: errno=%d err=%s\n",
-                argv0, errno, strerror(errno));
+        fprintf(stderr, "Error reading output of `%s`: errno=%d err=%s\n", argv,
+                errno, strerror(errno));
         return false;
     }
-    CHECK(read_bytes, <=, sizeof(buf), "%zu");
+    CHECK(read_bytes, <=, sizeof(output), "%zu");
 
-    printf("%.*s", (int)read_bytes, buf);
+    printf("%.*s", (int)read_bytes, output);
 
     if (pclose(exe_process) != 0) {
         return false;
