33582a472e13c1076c05cb3a92b6f63f1c01550c
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-12-22 08:21:01 +0100

    fix syscall

--- codegen.h
+++ codegen.h
@@ -409,8 +409,10 @@ static void emit_expr(const parser_t* parser, const int expr_i) {
 
             for (int i = len - 1; i > 0; i--) {
                 emit_expr(parser, syscall.sy_arg_nodes_i[i]);
-                println("mov %%rax, %s", fn_args[i - 1]);
+                println("push %%rax");
             }
+            for (int i = 1; i < len; i++) println("pop %s", fn_args[i - 1]);
+
             emit_expr(parser, syscall.sy_arg_nodes_i[0]);
             println("syscall");
             println("mov $0, %%rax");  // Reset return value for now
