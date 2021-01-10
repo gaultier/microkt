d11594fdbff68c3ebd163101736f70db6644a8c8
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-01-10 11:32:39 +0100

    Re-enable class test

--- tests/class.kt
+++ tests/class.kt
@@ -1,12 +1,13 @@
 class Empty{}
+// class Person{var id: Long = 99}
 
 fun main(){ 
-  //println(Empty()) // expect: Instance of size 0
-  //println(Empty()) // expect: Instance of size 0
+  println(Empty()) // expect: Instance of size 0
+  println(Empty()) // expect: Instance of size 0
   val e : Empty = Empty()
   println(e) // expect: Instance of size 0
 
-  //println(Person()) // expect: Instance of size 32
+  // println(Person()) // expect: Instance of size 32
 
 
   // WIP
